d932b9b6ebba07b4281f1983c053a3ebe5a896f5
Xavier Pilasi <pilasiha@gmail.com>
2024-10-13 22:53:23 +0200

    banner improving

--- src/app/screens/MainDefault.jsx
+++ src/app/screens/MainDefault.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import BannerHome from './components/home/BannerHome';
+
 
 const MainDefault = () => {
   return (
-    <div className='lg:min-h-[calc(100vh-5rem)] min-h-[calc(100vh-4rem)] mt-16 lg:mt-0 text-base text-textMainText bg-red-200 lg:px-20 px-5 py-10 flex-col  justify-center items-center'>
-
-        <h1 className='text-3xl font-riftBold text-textBigTitleGraphite '>Welcome to the RockVibes</h1>
-        <h2 className='text-5xl font-riftBold text-textBigTitlePink '>NEW FEATURES FOR CLIMBERS</h2>
-        <p className='py-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quisquam minus recusandae officiis, itaque reprehenderit sapiente laborum, sit doloremque consequuntur, iusto quo. Iste perspiciatis unde necessitatibus dicta doloremque quod quam?
-            </p></div>
+    
+    <div 
+      className='lg:min-h-screen   min-h-[calc(100vh-4rem)]   text-base text-textMainText bg-red-200 w-full  flex-col  '>
+        {/* BANNER */}
+        <BannerHome/>
+         
+          
+                
+    </div>
   )
 }
 
