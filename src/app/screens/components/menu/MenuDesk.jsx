5e31c6e6e189ca84af7995ba329e7e31ab7f1c7d
Xavier Pilasi <pilasiha@gmail.com>
2024-10-13 18:18:21 +0200

    nav efecto ok

--- src/app/screens/components/menu/MenuDesk.jsx
+++ src/app/screens/components/menu/MenuDesk.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const MenuDesk = () => {
+
+
   return (
     <div>
-      <ul className='flex lg:flex-row flex-col justify-center items-center'>
+      <ul className='flex lg:flex-row flex-col justify-center items-center '>
         <li className='py-2 lg:py-0 px-2'>Dónde escalar</li>
         <li className='py-2 lg:py-0 px-2'>Equipamento</li>
         <li className='py-2 lg:py-0 px-2'>Blog</li>
