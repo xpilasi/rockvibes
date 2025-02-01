866ec7d8eabd4dc7b19c6590adf429f3c4e94a60
xpilasi <pilasiha@gmail.com>
2025-02-01 21:42:58 +0100

    migration tailwind 4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import MainDefault from './mainComponents/MainDefault'
 
 export default function Home() {
-  return <MainDefault />
+  return (
+    <div className="p-4 bg-blue-500 text-white">
+      <h1 className="text-2xl font-bold">Test Tailwind</h1>
+    </div>
+  )
 } 
