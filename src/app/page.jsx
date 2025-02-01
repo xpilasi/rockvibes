a925896a25d106500a754f0b00cfe086ed93d400
xpilasi <pilasiha@gmail.com>
2025-02-02 00:42:45 +0100

    test3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import MainDefault from './mainComponents/MainDefault'
 export default function Home() {
   return (
     <div className="p-4 bg-blue-500 text-white">
-      <h1 className="text-2xl font-bold">Test Tailwind</h1>
+      <h1 className="text-2xl font-bold">Test Tailwind 1</h1>
     </div>
   )
 } 
