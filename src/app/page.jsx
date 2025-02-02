478a5f063e9a1ea89e5409517e04ad50f5467f4a
xpilasi <pilasiha@gmail.com>
2025-02-02 20:26:27 +0100

    test 3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="p-4 pt-30  text-white h-screen">
-      <h1 className="text-2xl font-bold">Test Tailwind 1</h1>
+      <h1 className="text-2xl font-bold">Test Tailwind 10</h1>
     </div>
   )
 } 
