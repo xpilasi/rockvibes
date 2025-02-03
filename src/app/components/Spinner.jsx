a019d665def06b2e988ab577f96a8a212451edaf
xpilasi <pilasiha@gmail.com>
2025-02-03 11:09:15 +0100

    test 4

--- src/app/components/Spinner.jsx
+++ src/app/components/Spinner.jsx
@@ -2,6 +2,7 @@ const Spinner = () => {
   return (
     <div className="flex items-center justify-center min-h-[400px]">
       <div className="relative">
+        <div className="text-2xl text-black text-3xl">SPINNER</div>
         <div className="w-12 h-12 rounded-full absolute border-4 border-solid border-gray-200"></div>
         <div className="w-12 h-12 rounded-full animate-spin absolute border-4 border-solid border-pink-500 border-t-transparent"></div>
       </div>
