ccacee68e0775adb0245cfc5925f85182a064dea
xpilasi <pilasiha@gmail.com>
2025-02-03 12:03:13 +0100

    test 7

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,23 @@
-import Nav from '@/components/Nav'
-import Footer from '@/components/Footer'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "RockVibes",
+  description: "Find your climbing spot",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Nav />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 } 
